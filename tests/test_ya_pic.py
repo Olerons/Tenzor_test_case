30534845ada7786f3a934c36ebc30f6af7007193
Olerons <ser1a@ya.ru>
2023-06-21 10:20:29 +0300

    Логирование через allure

--- tests/test_ya_pic.py
+++ tests/test_ya_pic.py
@@ -1,27 +1,39 @@
 from YandexPages import YaPicPage
+import allure
+import logging
 
 href_first_pic = ''
 
+
+@allure.step("Проверяем, появляется ли кнопка с сервисами Яндекс")
 def test_menu_button(browser):
     yandex_main = YaPicPage(browser)
     yandex_main.to_site()
     assert yandex_main.check_service_btns()
 
+
+@allure.step("Проверяем, получилось ли перейти на сервис 'картинки' при нажатии на кнопку")
 def test_open_ya_pic(browser):
     yandex_main = YaPicPage(browser)
     yandex_main.click_service_pic()
     assert browser.current_url == 'https://yandex.ru/images/'
 
+
+@allure.step("Проверяем, отображается ли в поле поиска название категории картинок, при нажатии")
 def test_category_name(browser):
     yandex_main = YaPicPage(browser)
     category_name = yandex_main.check_popular_category()
     assert category_name[0] == category_name[1]
 
+
+@allure.step("Проверяем, отображается ли картинка, при нажатии на нее")
 def test_open_pic(browser):
     yandex_main = YaPicPage(browser)
     picture = yandex_main.open_first_pic()
     assert picture.is_displayed()
 
+
+@allure.step("Проверяем, меняется ли картинка, при переходе к следующей")
 def test_open_second_pic(browser):
     global href_first_pic
 
@@ -31,6 +43,8 @@ def test_open_second_pic(browser):
     second_pic = yandex_main.get_original_pic()
     assert href_first_pic != second_pic
 
+
+@allure.step("Проверяем, возвращается ли та же самая картинка, при переходе назад")
 def test_check_prev_pic(browser):
     global href_first_pic
 
--- tests/test_ya_search.py
+++ tests/test_ya_search.py
@@ -1,25 +1,36 @@
 from YandexPages import  YaSearchPage
+import allure
+import logging
 
 
+@allure.step("Проверяем, появляется поле ввода запросов на ya.ru")
 def test_search_field(browser):
     yandex_main = YaSearchPage(browser)
     yandex_main.to_site()
     assert yandex_main.check_search_field()
 
+
+@allure.step("Проверяем, можем ли мы ввести поисковый запрос в поле запросов")
 def test_input_word(browser):
     yandex_main = YaSearchPage(browser)
     assert yandex_main.enter_word("Тензор")
 
+
+@allure.step("Проверяем, появляются ли подсказки при вводе запроса")
 def test_suggest_list(browser):
     yandex_main = YaSearchPage(browser)
     elements_suggest = yandex_main.view_suggest_field()
     assert elements_suggest
 
+
+@allure.step("Проверяем, отображается ли страница с результатами поиска")
 def test_search_result(browser):
     yandex_main = YaSearchPage(browser)
     yandex_main.click_search_button()
     assert yandex_main.view_search_result()
 
+
+@allure.step("Проверяем, что первая ссылка (не считая рекламы) ведет на tensor.ru")
 def test_first_href(browser):
     yandex_main = YaSearchPage(browser)
     first_href = yandex_main.view_first_href()
